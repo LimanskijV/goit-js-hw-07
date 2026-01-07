eda4507ca9b16a7099b95b5954ebcb09e8b7b012
Valentin_Limanskij <valentin.limanskij@gmail.com>
2026-01-07 10:11:02 +0200

    newUpd1

--- js/task-3.js
+++ js/task-3.js
@@ -1,6 +1,10 @@
 const innerText = document.querySelector('#name-input')
 const outerText = document.querySelector("#name-output")
 
+outerText.addEventListener("input", () => {
+    outerText.value = outerText.value.trim();
+}
+)
 innerText.addEventListener("input", (event)=> {
     if(innerText.value.trim() === ""){
         outerText.textContent = "Anonymous";
